e3f8e3c430d630d80ba7990d16e0e76e35cb5de1
ndurbin <nick.durbin@gmail.com>
2019-08-06 23:04:29 -0400

    Brought all the files over form the old repo.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM \n Is \n Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -37,6 +37,81 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+// Image Updates
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = "./img/logo.png";
+
+let cta = document.getElementById("cta-img");
+cta.src = "./img/header-img.png";
+
+let midImg = document.getElementById("middle-img");
+midImg.src = "./img/mid-page-accent.jpg";
+
+// Navigation 
+let navItems = document.querySelectorAll('a');
+
+navItems[0].innerText = siteContent['nav']['nav-item-1'];
+navItems[1].innerText = siteContent['nav']['nav-item-2']; 
+navItems[2].innerText = siteContent['nav']['nav-item-3'];
+navItems[3].innerText = siteContent['nav']['nav-item-4'];
+navItems[4].innerText = siteContent['nav']['nav-item-5'];
+navItems[5].innerText = siteContent['nav']['nav-item-6'];
+
+navItems.forEach(link => link.style.color = 'green');
+
+// Call To Action Content
+let ctaHeader = document.querySelector('h1');
+ctaHeader.innerText = siteContent['cta']['h1'];
+
+let ctaButton = document.querySelector('button');
+ctaButton.textContent = siteContent['cta']['button'];
+
+// Top Content of the Main Section
+let topFeaturesH4 = document.querySelectorAll('h4')[0];
+topFeaturesH4.textContent = siteContent['main-content']['features-h4'];
+
+let topFeaturesP = document.querySelectorAll('p')[0];
+topFeaturesP.textContent = siteContent['main-content']['features-content'];
+
+let topAboutH4 = document.querySelectorAll('h4')[1];
+topAboutH4.textContent = siteContent['main-content']['about-h4'];
+
+let topAboutP = document.querySelectorAll('p')[1];
+topAboutP.textContent = siteContent['main-content']['about-content'];
+
+// Bottom Content of the Main Section
+let bottomServicesH4 = document.querySelectorAll('h4')[2];
+bottomServicesH4.textContent = siteContent['main-content']['services-h4'];
+
+let bottomServicesP = document.querySelectorAll('p')[2];
+bottomServicesP.textContent = siteContent['main-content']['services-content'];
+
+let bottomProductH4 = document.querySelectorAll('h4')[3];
+bottomProductH4.textContent = siteContent['main-content']['product-h4'];
+
+let bottomProductP = document.querySelectorAll('p')[3];
+bottomProductP.textContent = siteContent['main-content']['product-content'];
+
+let bottomVisionH4 = document.querySelectorAll('h4')[4];
+bottomVisionH4.textContent = siteContent['main-content']['vision-h4'];
+
+let bottomVisionP = document.querySelectorAll('p')[4];
+bottomVisionP.textContent = siteContent['main-content']['vision-content'];
+
+// Contact Content
+let contactH4 = document.querySelectorAll('h4')[5];
+contactH4.textContent = siteContent['contact']['contact-h4'];
+
+let addressP = document.querySelectorAll('p')[5];
+addressP.textContent = siteContent['contact']['address'];
+
+let phone4 = document.querySelectorAll('p')[6];
+phone4.textContent = siteContent['contact']['phone'];
+
+let emailP = document.querySelectorAll('p')[7];
+emailP.textContent = siteContent['contact']['email'];
+
+// Footer Content
+let footerContent = document.getElementsByTagName('p')[8];
+footerContent.textContent = siteContent['footer']['copyright'];
+footerContent.style.fontWeight = '600';
