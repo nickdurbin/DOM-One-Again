15a0499d001cc5e0eaf4986b0307ebcc20de5e50
ndurbin <nick.durbin@gmail.com>
2019-08-08 00:03:12 -0400

    Added the logic to the digital timer and stored the value to an onlick element for a start button. Reset does reset the digits, but unable to start again afterwards.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,13 +1,3 @@
-const startBtn = document.querySelector('.start-btn');
-const resetBtn = document.querySelector('.reset-btn');
-const digits = document.querySelector('.digits');
-const digit = document.querySelectorAll('.digit');
-const tenMs = document.getElementById('msTens');
-const hundredMs = document.getElementById('msHundreds');
-const seconds = document.getElementById('secondOnes');
-const tenSeconds = document.getElementById('secondTens');
-
-
 // Need to run a loop through the tens of milliseconds (0-9) to get to hundreds of milliseconds
 // Run the same loop through hundreds (0-9) to get to seconds
 // Same loop to get to tens of seconds, but stop on 10 and turn red.
@@ -18,20 +8,76 @@ const tenSeconds = document.getElementById('secondTens');
 // 1000ms ='s 1 second.
 // Change all digit element classes to 0
 
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const digits = document.querySelector('.digits');
+const digit = document.querySelectorAll('.digit');
+const tenMs = document.getElementById('msTens');
+const hundredMs = document.getElementById('msHundreds');
+const seconds = document.getElementById('secondOnes');
+const tenSeconds = document.getElementById('secondTens');
 
-
-startBtn.addEventListener('onclick', function (){
-  if (startBtn.getAttribute("button") == startBtn.innerHTML) {
-    startBtn.innerHTML = StartBtn.getAttribute("button");
-  } else {
-    startBtn.setAttribute("button", startBtn.innerHTML);
-    startBtn.innerHTML = startBtn.getAttribute("button");
+startBtn.onclick = function startTimer () {
+  init();
+  
+  function init () {
+    let ms = 0;
+    tenSeconds.innerHTML = "0";
+    seconds.innerHTML = "0";
+    hundredMs.innerHTML = "0";
+    tenMs.innerHTML = "0";
+    
+    const timerInterval = window.setInterval(() => {
+      ms += 10;
+      if (ms === 10000) {
+        endTimer(timerInterval); 
+      }
+      updateTimer(ms);
+    }, 10);
   }
-}, false);
+  
+  function increment (string) {
+    let number = Number(string) + 1;
+    return number.toString();
+  }
 
-// startBtn.onclick = startTimer;
+  function endTimer (intervalId) {
+    clearInterval(intervalId);
+    Array.from(digits.children).forEach(digit => {
+      digit.classList.add("redDigit");
+    })
+  }
+
+  function updateTimer (ms) {
+    if (ms === 10000) {
+      tenSeconds.innerHTML = "1";
+      seconds.innerHTML = "0";
+      hundredMs.innerHTML = "0";
+      tenMs.innerHTML = "0";
+    } else if (ms % 1000 === 0) {
+      seconds.innerHTML = increment(seconds.innerHTML);
+      hundredMs.innerHTML = "0";
+      tenMs.innerHTML = "0";
+    } else if (ms % 100 === 0) {
+      hundredMs.innerHTML = increment(hundredMs.innerHTML);
+      tenMs.innerHTML = "0";
+    } else {
+      tenMs.innerHTML = increment(tenMs.innerHTML);
+    }
+  }
+}
+
+
+// startBtn.addEventListener('onclick', function (){
+//   if (startBtn.getAttribute("button") == startBtn.innerHTML) {
+//     startBtn.innerHTML = StartBtn.getAttribute("button");
+//   } else {
+//     startBtn.setAttribute("button", startBtn.innerHTML);
+//     startBtn.innerHTML = startBtn.getAttribute("button");
+//   }
+// }, false);
 
 resetBtn.onclick = function () {
-  digits.textContent = "00:00";
+  digits.textContent = "--:--";
   seconds = 0; ms = 0;
 }
